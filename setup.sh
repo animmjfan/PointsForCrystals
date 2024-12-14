3bae8c81374091b3f7104c3d8afa03a1d45f1d4c
Animmjfan <anthonyfalzon12@icloud.com>
2024-12-14 23:30:21 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ echo -e "1 - Full setup\n2 - Small setup\n3 - custom setup\n"
 echo -e "Choose: "
 read setuptype
 if [ $setuptype == 1 ]; then
-    $installdir = /usr/bin/animmfan/PointsForCrystals/
+    $installdir = /usr/local/bin/animmfan/PointsForCrystals/
     mkdir -p $installdir
+    cp PFC $installdir
     cp PointsForCrystals $installdir
-    cp PFC.py $installdir
+    
