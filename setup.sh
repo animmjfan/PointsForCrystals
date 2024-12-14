e16541c63a27f8bf2769d7e9b6dce2f361d94fc4
Animmjfan <anthonyfalzon12@icloud.com>
2024-12-14 17:56:12 +1100

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+^L
+echo -e "1 - Full setup\n2 - Small setup\n3 - custom setup\n"
+echo -e "Choose: "
+read setuptype
+if [ $setuptype == 1 ]; then
+    $installdir = /usr/bin/PointsForCrystals/
+    mkdir -p $installdir
+    cp runmypyapp.sh $installdir
+    cp PointsForCrystals.py $installdir
